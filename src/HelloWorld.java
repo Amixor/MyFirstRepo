525b8ec5f14bdfa95c1159c3b26a19438893c12d
Ashutosh Pitkar <pitkaram@miamioh.edu>
2020-05-01 05:35:41 -0400

    My first command in Git

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * This just a tester class to check if I can use Git.
+ * @author Ashutosh Pitkar
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
